b51b80011cbdc74780883fc95aa4a46f31eb18e2
Hephzisoft <hephzisoft@gmail.com>
2023-12-19 10:05:20 +0100

    changesd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,9 @@
 
 const Hero = () => {
   return (
-    <div className="bg-hero mb-10 h-[100vh] w-full bg-cover bg-center ">
-        
+    <div className="bg-hero mb-10 h-[100vh] flex w-full bg-cover bg-center items-center text-center justify-center ">
+        <p className="font-caveat font-bold text-white text-center  text-2xl ">all natural good for your health</p>
+        <h3>Health Benefit, </h3>
     </div>
   )
 }
